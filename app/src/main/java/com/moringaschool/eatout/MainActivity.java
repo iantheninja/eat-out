d1fb7cf36b6fa6d4d97814a72def6beeb6d26e5b
Ian Munene <munene.ian@live.com>
2017-08-16 14:48:09 +0300

    font added to layout.

--- app/src/main/java/com/moringaschool/eatout/MainActivity.java
+++ app/src/main/java/com/moringaschool/eatout/MainActivity.java
@@ -1,11 +1,13 @@
 package com.moringaschool.eatout;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     //public static final String TAG = MainActivity.class.getSimpleName();
@@ -17,8 +19,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView tvEatOut;
+
+        tvEatOut = (TextView) findViewById(R.id.tv_eat_out);
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrichregular.ttf");
+        tvEatOut.setTypeface(ostrichFont);
+
         mFindZipEditText = (EditText) findViewById(R.id.et_zip_code);
         mFindRestaurantButton = (Button) findViewById(R.id.btn_find_restaurant);
+
         mFindRestaurantButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/moringaschool/eatout/RestaurantsActivity.java
+++ app/src/main/java/com/moringaschool/eatout/RestaurantsActivity.java
@@ -3,10 +3,13 @@ package com.moringaschool.eatout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class RestaurantsActivity extends AppCompatActivity {
     TextView mTvLocation;
+    ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +21,10 @@ public class RestaurantsActivity extends AppCompatActivity {
 
         String loc = getString(R.string.location_text, zip);
         mTvLocation.setText(loc);
-        // place holder set up from the strings resource file.
-        // zip will be concatenated to location text
+
+        mListView = (ListView) findViewById(R.id.lv_restaurants);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        mListView.setAdapter(adapter);
+
     }
 }
